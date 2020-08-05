7a20e12443e2bf4f26873a126da9dec0391c5817
Abd-Lillah-EXT <uic57569@contiwan.com>
2020-08-05 15:55:27 +0200

    Component(Core): UI Toolbar done

--- src/components/core/Toolbar/Toolbar.vue
+++ src/components/core/Toolbar/Toolbar.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="toolbar-layout">
-    Toolbar  dddszeee
+    <div class="content">
+      <div class="logo-layout">
+        <img src="./assets/blog-generic-logo.png" class="logo" />
+        <div class="title">
+          VUEBLOG
+        </div>
+      </div>
+      <button class="button dark">
+        <log-out-icon size="1.5x" class="mx-2" />
+        <span>
+          Sign out
+        </span>
+      </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
+import { LogOutIcon } from 'vue-feather-icons';
 import './styles/toolbar.scss';
 
 @Component({
   components: {
+    LogOutIcon,
   },
 })
 export default class Toolbar extends Vue {
--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,9 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        styles: path.resolve(__dirname, 'src/assets/styles'),
+        styles: path.resolve(__dirname, 'src/styles'),
         utils: path.resolve(__dirname, 'src/utils'),
+        assets: path.resolve(__dirname, 'src/assets'),
         src: path.resolve(__dirname, 'src'),
       },
     },
