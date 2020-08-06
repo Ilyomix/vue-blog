0b3a47d0c08c4223a00ebace0153be1d69d063e1
Abd-Lillah-EXT <uic57569@contiwan.com>
2020-08-06 14:47:50 +0200

    Login: 401 Interceptor, and auth added to the header when logged, and removed when 401 or when the user is disconnected

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,22 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+Vue.http.interceptors.push((request: any, next: any) => {
+  if (localStorage['user-token']) {
+    request.headers.set('X-Authorization-Token', localStorage['user-token']);
+  }
+
+  next((response: any) => {
+    if (response.status === 401) {
+      localStorage.removeItem('user-token');
+      router.push({ name: '/login'});
+    }
+  });
+});
 
 Vue.config.productionTip = false;
 
